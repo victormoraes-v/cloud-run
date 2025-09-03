b1d3009961e21b7c4a803f8154173eff50e88d73
Victor Soares de Moraes <victor.moraes@dvenancio.local>
2025-09-03 10:40:02 -0300

    inclusao arquivo canal vendas

--- ingestao-arquivos/processors/__init__.py
+++ ingestao-arquivos/processors/__init__.py
@@ -4,7 +4,8 @@ from . import (
     info_lojas, 
     dark_stores, 
     info_lojas_servicos_farmaceuticos,
-    produtos_pricepoint
+    produtos_pricepoint,
+    canal_vendas
 )
 #from . import default_processor # Importante ter um fallback
 
@@ -14,14 +15,15 @@ PROCESSOR_MAP = {
     'Info Lojas.xlsx': info_lojas,
     "Darks_Store_Lojas.xlsx": dark_stores,
     "Info Lojas Servicos Farmaceuticos.xlsx": info_lojas_servicos_farmaceuticos,
-    "PRICEPOINT_CADASTRO PRODUTO_v2.xlsx": produtos_pricepoint
+    "PRICEPOINT_CADASTRO PRODUTO_v2.xlsx": produtos_pricepoint,
+    "Canal de Vendas.xlsx": canal_vendas
 }
 
 def get_processor(filename):
     """Encontra e retorna a função de processamento correta."""
     processor_module = PROCESSOR_MAP.get(filename)
-    logging.info(f"Processador específico '{processor_module.__name__}' encontrado para o arquivo '{filename}'.")
     if processor_module:
+        logging.info(f"Processador específico '{processor_module.__name__}' encontrado para o arquivo '{filename}'.")
         return processor_module.process
     # for key, processor_module in PROCESSOR_MAP.items():
     #     if key in filename:
---
+++ ingestao-arquivos/processors/canal_vendas.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import io
+import logging
+
+def process(file_bytes):
+    """Processa o arquivo 'Canal de Vendas.xlsx'."""
+    logging.info(f"Dentro do processador '{__name__}': aplicando regras específicas.")
+    
+    df = pd.read_excel(io.BytesIO(file_bytes), engine='openpyxl')
+    logging.info("Arquivo Excel lido com sucesso.")
+        
+    return df
